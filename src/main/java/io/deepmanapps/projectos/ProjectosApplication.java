549e71940b183dc1784e616b57fcb06945e9fd5f
deepmanapps <zahrouni.dhafer@gmail.com>
2026-07-13 21:23:58 +0200

    add back-end springboot

---
+++ src/main/java/io/deepmanapps/projectos/ProjectosApplication.java
@@ -0,0 +1,13 @@
+package io.deepmanapps.projectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/deepmanapps/projectos/ProjectosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.deepmanapps.projectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
